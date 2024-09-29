7666f4d86b5283b3d0e3be15c72fe01e9b975b5e
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-09-29 11:06:45 +0530

    Java Assignments

---
+++ Vowel.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Vowel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(ch + " is a vowel.");
+        } else {
+            System.out.println(ch + " is not a vowel.");
+        }
+    }
+}
