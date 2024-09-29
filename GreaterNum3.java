2fdb55ba454130d590ff39b55a19ce63ab3a8226
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-09-29 11:03:13 +0530

    Java Assignments

---
+++ GreaterNum3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GreaterNum3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a 1st number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter a 2nd number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter a 2nd number: ");
+        int num3 = scanner.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("The 1st number is the largest: " + num1);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("The 2nd number is the largest: " + num2);
+        } else {
+            System.out.println("The 3rd number is the largest: " + num3);
+        }
+
+    }
+}
