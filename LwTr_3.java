65534bac86582edf89e3e6ae5276146f21a906cd
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-09-29 15:44:57 +0530

    symbols logics

---
+++ LwTr_3.java
@@ -0,0 +1,10 @@
+public class LwTr_3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("3");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ UpTr_8.java
@@ -0,0 +1,10 @@
+public class UpTr_8 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i + 1; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+    }
+}
