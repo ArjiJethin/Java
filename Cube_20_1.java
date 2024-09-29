aab2275b5ebfadab42897642e0bd94769bb24844
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-09-29 12:16:06 +0530

    java assignments

---
+++ Cube_20_1.java
@@ -0,0 +1,12 @@
+public class Cube_20_1 {
+    public static void main(String[] args) {
+        int n = 20;
+
+        for (int i = n; i >= 1; i--) {
+            int cube = i * i * i;
+            System.out.print(cube + " ");
+        }
+
+    }
+
+}
