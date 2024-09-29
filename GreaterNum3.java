d560ff215d5aaba2a4043c59319525b76dbe5851
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-09-29 11:09:45 +0530

    Minor Error Fixes

--- GreaterNum3.java
+++ GreaterNum3.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 public class GreaterNum3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a 1st number: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Enter a 2nd number: ");
-        int num2 = scanner.nextInt();
-        System.out.print("Enter a 2nd number: ");
-        int num3 = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a 1st number: ");
+            int num1 = scanner.nextInt();
+            System.out.print("Enter a 2nd number: ");
+            int num2 = scanner.nextInt();
+            System.out.print("Enter a 2nd number: ");
+            int num3 = scanner.nextInt();
 
-        if (num1 > num2 && num1 > num3) {
-            System.out.println("The 1st number is the largest: " + num1);
-        } else if (num2 > num1 && num2 > num3) {
-            System.out.println("The 2nd number is the largest: " + num2);
-        } else {
-            System.out.println("The 3rd number is the largest: " + num3);
+            if (num1 > num2 && num1 > num3) {
+                System.out.println("The 1st number is the largest: " + num1);
+            } else if (num2 > num1 && num2 > num3) {
+                System.out.println("The 2nd number is the largest: " + num2);
+            } else {
+                System.out.println("The 3rd number is the largest: " + num3);
+            }
         }
 
     }
--- PositiveNum.java
+++ PositiveNum.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class PositiveNum {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
-        if (num > 0) {
-            System.out.println("The number is positive.");
-        } else {
-            System.out.println("The number is negative.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int num = scanner.nextInt();
+            if (num > 0) {
+                System.out.println("The number is positive.");
+            } else {
+                System.out.println("The number is negative.");
+            }
         }
     }
 }
