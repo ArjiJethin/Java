811792356cf7b7c4b35a9d746f87a60562ed8696
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-09-29 12:10:08 +0530

    Java Assignmnts

---
+++ M5thTable.java
@@ -0,0 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class M5thTable {
+    public M5thTable() {
+    }
+
+    public static void main(String[] var0) {
+        byte var1 = 5;
+        System.out.println("Descending Multiplication table of " + var1 + "\n");
+
+        for (int var2 = 10; var2 >= 1; --var2) {
+            System.out.println("" + var1 + " * " + var2 + " = " + var1 * var2);
+        }
+
+    }
+}
