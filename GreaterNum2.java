c3f766a2a681e6e9ac55dc1d69c67aa4a2000141
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-09-29 11:00:42 +0530

    Java Assignments

---
+++ GreaterNum2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class GreaterNum2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a 1st number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter a 2nd number: ");
+        int num2 = scanner.nextInt();
+        System.out.println("The greater number is: " + (num1 > num2 ? num1 : num2));
+
+    }
+}
