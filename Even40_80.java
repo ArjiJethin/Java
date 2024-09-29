894273d34e1a8540b126281751530c339a6f6338
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-09-29 12:02:21 +0530

    Java Assignments

---
+++ Even40_80.java
@@ -0,0 +1,11 @@
+public class Even40_80 {
+
+    public static void main(String[] args) {
+        for (int i = 40; i <= 80; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+}
