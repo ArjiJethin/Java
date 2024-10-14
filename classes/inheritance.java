ab1b9626f0d1e48f29b2aad98235f0a9ca5ce2c2
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-10-14 19:01:55 +0530

    changes

---
+++ classes/inheritance.java
@@ -0,0 +1,11 @@
+package classes;
+
+// class subclass extends superclass{
+//     //body of class
+// }
+
+public class inheritance {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
