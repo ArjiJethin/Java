a0158503ce676243b954d3784d2956fdca15a626
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-16 12:01:52 +0530

    Add Problem8.java to get user input and display formatted numbers

---
+++ Talentely/Interactions1/Problem8.java
@@ -0,0 +1,16 @@
+package Talentely.Interactions1;
+
+import java.util.*;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x;
+        System.out.print("Enter the value of x: ");
+        x = sc.nextDouble();
+        sc.close();
+        System.out.printf("%e", x);
+    }
+}
+
+// 2 Marks
