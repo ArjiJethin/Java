7b443f5ddcb2b010e805ce2a1a7cfd4fec7f4ab8
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-16 10:29:52 +0530

    Add Problem8.java to print two numbers

---
+++ Talentely/Problem8.java
@@ -0,0 +1,9 @@
+package Talentely;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("The two numbers are " + a + " and " + b);
+    }
+}
