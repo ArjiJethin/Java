20390a20c94f7111f635cb07cd8e2d90fc281765
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-16 10:30:04 +0530

    Add Problem9.java to get user input and display formatted numbers

---
+++ Talentely/Problem9.java
@@ -0,0 +1,20 @@
+package Talentely;
+
+import java.util.*;
+
+public class Problem9 {
+    public static void main(String[] args) {
+        long a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        a = sc.nextLong();
+        System.out.print("Enter the second number: ");
+        b = sc.nextLong();
+        sc.close();
+
+        System.out.printf("%05d\n", a);
+        System.out.printf("%5d\n", b);
+    }
+}
+
+// 2 Marks
