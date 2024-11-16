1dffcff3f1c80fd2668824260b4eda6d7e1df2e0
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-16 11:31:22 +0530

    Add Problem5.java to get user input and display formatted numbers

---
+++ Talentely/Interactions1/Problem5.java
@@ -0,0 +1,17 @@
+package Talentely.Interactions1;
+
+import java.util.*;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float x;
+        int n;
+        System.out.print("Enter a number: ");
+        x = sc.nextFloat();
+        System.out.print("Enter the precision number: ");
+        n = sc.nextInt();
+        sc.close();
+        System.out.printf("%." + n + "f", x);
+    }
+}
