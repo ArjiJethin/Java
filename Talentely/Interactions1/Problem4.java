9eef3326ff3078d1d50fc2f82d879480f7e9f832
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-16 11:24:38 +0530

    Add Problem4.java to get user input and display formatted numbers

---
+++ Talentely/Interactions1/Problem4.java
@@ -0,0 +1,14 @@
+package Talentely.Interactions1;
+
+import java.util.*;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n;
+        System.out.print("Enter the value of n: ");
+        n = sc.nextDouble();
+        sc.close();
+        System.out.printf("%20.4f", n);
+    }
+}
