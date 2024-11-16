1689ffe16a22c59df62a2613357554be01c1529c
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-16 10:29:16 +0530

    Add Problem7.java to get user input and display it

---
+++ Talentely/Problem7.java
@@ -0,0 +1,16 @@
+package Talentely;
+
+import java.util.*;
+
+public class Problem7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch;
+        System.out.print("Enter a character: ");
+        ch = sc.next().charAt(0);
+        sc.close();
+        System.out.println("The given character is: " + ch);
+    }
+}
+
+// 2 Marks
