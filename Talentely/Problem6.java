9c2d4204e396023839d75df6649f0158242cc1ab
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-16 09:52:54 +0530

    Add Problem6.java to get user input and display it

---
+++ Talentely/Problem6.java
@@ -0,0 +1,14 @@
+package Talentely;
+
+import java.util.*;
+
+public class Problem6 {
+    public static void main(String[] args) {
+        long data;
+        System.out.print("Enter a Number: ");
+        Scanner sc = new Scanner(System.in);
+        data = sc.nextLong();
+        sc.close();
+        System.out.println("The number given in the input is: " + data);
+    }
+}
