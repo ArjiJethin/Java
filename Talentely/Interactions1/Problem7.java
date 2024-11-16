f824b621780fa6e1861bcccb11aa7d88a2409396
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-16 12:12:48 +0530

    Refactor Problem7 to get user input and display formatted numbers

--- Talentely/Interactions1/Problem7.java
+++ Talentely/Interactions1/Problem7.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Problem7 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str;
-        str = sc.next();
-        sc.close();
-        long n = Long.parseLong(str.substring(2, 16));
-        System.out.println(n);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a hexadecimal number: ");
+        String hexInput = scanner.nextLine();
+        int decimalOutput = Integer.parseInt(hexInput, 16);
+        System.out.println("The integer value is: " + decimalOutput);
+        scanner.close();
     }
 }
 
 /*
- * Scanner scanner = new Scanner(System.in);
- * System.out.print("Enter a hexadecimal number: ");
- * String hexInput = scanner.nextLine();
- * int decimalOutput = Integer.parseInt(hexInput, 16);
- * System.out.println("The integer value is: " + decimalOutput);
- * scanner.close();
+ * Scanner sc = new Scanner(System.in);
+ * String str;
+ * str = sc.next();
+ * sc.close();
+ * long n = Long.parseLong(str.substring(2, 16));
+ * System.out.println(n);
  */
 
 // 2 Marks
