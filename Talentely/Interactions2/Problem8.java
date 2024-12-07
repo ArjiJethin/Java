9f74d1b8eba54af758026fde1122aa797d4d3e38
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-12-07 15:52:39 +0530

    Add Problem8.java to calculate the absolute value of a given number

---
+++ Talentely/Interactions2/Problem8.java
@@ -0,0 +1,12 @@
+package Talentely.Interactions2;
+
+import java.util.*;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int num = sc.nextInt();
+        System.out.println(Math.abs(num));
+    }
+}
