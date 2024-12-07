2606fdd9a0241b0445a46d5a5d5f6cd71415bdb1
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-12-07 15:52:26 +0530

    Add Problem7.java to calculate the difference between 3 and a given number

---
+++ Talentely/Interactions2/Problem7.java
@@ -0,0 +1,13 @@
+package Talentely.Interactions2;
+
+import java.util.*;
+
+public class Problem7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        int result = 3 - num;
+        System.out.println(result);
+    }
+}
