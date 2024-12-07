25c9d7302c7aec90e37f9d466cf50a7f9057fd4b
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-12-07 16:01:01 +0530

    Add Problem9.java to convert power from kilowatts to horsepower

---
+++ Talentely/Inteactions3/Problem9.java
@@ -0,0 +1,13 @@
+package Talentely.Inteactions3;
+
+import java.util.*;
+
+public class Problem9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        double kw = sc.nextDouble();
+        double hp = kw * 1.35962;
+        System.out.printf("%.2f", hp);
+    }
+}
