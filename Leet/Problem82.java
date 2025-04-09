51659c8e0fc51427faa63d957034283802aaa3e3
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-09 20:34:44 +0530

    Implement Problem82.java to remove duplicates from a linked list

---
+++ Leet/Problem82.java
@@ -0,0 +1,32 @@
+public class Problem82 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1,
+                new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5)))))));
+        Solution solution = new Solution();
+        ListNode result = solution.deleteDuplicates(head);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur.next != null && cur.next.val == cur.val) {
+                cur = cur.next;
+            }
+            if (pre.next == cur) {
+                pre = cur;
+            } else {
+                pre.next = cur.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
