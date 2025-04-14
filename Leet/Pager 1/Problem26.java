d32e595b4fe7e6264f5aefdce50725f09cc0c596
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-14 10:01:36 +0530

    Add solution for removing duplicates from sorted array

---
+++ Leet/Pager 1/Problem26.java
@@ -0,0 +1,24 @@
+
+public class Problem26 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 1, 1, 2 };
+        int k = solution.removeDuplicates(nums);
+        System.out.println(k);
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        for (int x : nums) {
+            if (k == 0 || x != nums[k - 1]) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+}
