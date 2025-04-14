5d298593a9bad92454393ddc0f0c5b50aa25583a
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-14 10:01:30 +0530

    Add solution for reversing linked list in groups of K

---
+++ Leet/Pager 1/Problem25.java
@@ -0,0 +1,47 @@
+
+public class Problem25 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        int k = 2;
+        ListNode result = solution.reverseKGroup(head, k);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy, cur = dummy;
+        while (cur.next != null) {
+            for (int i = 0; i < k && cur != null; ++i) {
+                cur = cur.next;
+            }
+            if (cur == null) {
+                return dummy.next;
+            }
+            ListNode t = cur.next;
+            cur.next = null;
+            ListNode start = pre.next;
+            pre.next = reverseList(start);
+            start.next = t;
+            pre = start;
+            cur = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null, p = head;
+        while (p != null) {
+            ListNode q = p.next;
+            p.next = pre;
+            pre = p;
+            p = q;
+        }
+        return pre;
+    }
+}
