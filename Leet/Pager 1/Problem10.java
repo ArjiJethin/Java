e44443c71ac85d5706d855657a22d4e77c0d6803
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-14 09:59:58 +0530

    Add solution for matching regular expression

---
+++ Leet/Pager 1/Problem10.java
@@ -0,0 +1,32 @@
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "aab";
+        String p = "c*a*b";
+        boolean result = solution.isMatch(s, p);
+        System.out.println("Is match: " + result);
+    }
+}
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (i > 0 && (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1))) {
+                        f[i][j] |= f[i - 1][j];
+                    }
+                } else if (i > 0
+                        && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1))) {
+                    f[i][j] = f[i - 1][j - 1];
+                }
+            }
+        }
+        return f[m][n];
+    }
+}
