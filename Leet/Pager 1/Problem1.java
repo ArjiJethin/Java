ddaa683cb8531f5a3ea3e07e7ea37afb9ab945dd
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-14 09:59:25 +0530

    Refactor Problem1.java to optimize twoSum method and add missing imports

---
+++ Leet/Pager 1/Problem1.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0;; ++i) {
+            int x = nums[i];
+            int y = target - x;
+            if (m.containsKey(y)) {
+                return new int[] { m.get(y), i };
+            }
+            m.put(x, i);
+        }
+    }
+}
