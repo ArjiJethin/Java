31e7aed95959f96e2135ea2fec2ee6aad8a2bf5a
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-14 10:00:49 +0530

    Add solution for finding four numbers that sum to a target

---
+++ Leet/Pager 1/Problem18.java
@@ -0,0 +1,53 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem18 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 1, 0, -1, 0, -2, 2 };
+        int target = 0;
+        List<List<Integer>> result = solution.fourSum(nums, target);
+        System.out.println("Four Sum: " + result);
+    }
+}
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (n < 4) {
+            return ans;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int k = j + 1, l = n - 1;
+                while (k < l) {
+                    long x = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if (x < target) {
+                        ++k;
+                    } else if (x > target) {
+                        --l;
+                    } else {
+                        ans.add(List.of(nums[i], nums[j], nums[k++], nums[l--]));
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            ++k;
+                        }
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            --l;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
