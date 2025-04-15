c0d525da979db9c062f9c940e4171862f5fba37b
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-15 18:54:15 +0530

    Add solution for solving the N-Queens problem

---
+++ Leet/Pager 1/Problem51.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Problem51 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 4;
+        List<List<String>> result = solution.solveNQueens(n);
+        for (List<String> list : result) {
+            for (String s : list) {
+                System.out.println(s);
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution {
+    private List<List<String>> ans = new ArrayList<>();
+    private int[] col;
+    private int[] dg;
+    private int[] udg;
+    private String[][] g;
+    private int n;
+
+    public List<List<String>> solveNQueens(int n) {
+        this.n = n;
+        col = new int[n];
+        dg = new int[n << 1];
+        udg = new int[n << 1];
+        g = new String[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(g[i], ".");
+        }
+        dfs(0);
+        return ans;
+    }
+
+    private void dfs(int i) {
+        if (i == n) {
+            List<String> t = new ArrayList<>();
+            for (int j = 0; j < n; ++j) {
+                t.add(String.join("", g[j]));
+            }
+            ans.add(t);
+            return;
+        }
+        for (int j = 0; j < n; ++j) {
+            if (col[j] + dg[i + j] + udg[n - i + j] == 0) {
+                g[i][j] = "Q";
+                col[j] = dg[i + j] = udg[n - i + j] = 1;
+                dfs(i + 1);
+                col[j] = dg[i + j] = udg[n - i + j] = 0;
+                g[i][j] = ".";
+            }
+        }
+    }
+}
