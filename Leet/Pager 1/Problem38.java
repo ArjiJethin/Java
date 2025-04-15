1c69e3acbf22ea8eb6d4e570deaccef6bc154e95
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-15 18:52:20 +0530

    Add solution for counting and saying a sequence

---
+++ Leet/Pager 1/Problem38.java
@@ -0,0 +1,27 @@
+
+public class Problem38 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countAndSay(4)); // Output: "1211"
+    }
+}
+
+class Solution {
+    public String countAndSay(int n) {
+        String s = "1";
+        while (--n > 0) {
+            StringBuilder t = new StringBuilder();
+            for (int i = 0; i < s.length();) {
+                int j = i;
+                while (j < s.length() && s.charAt(j) == s.charAt(i)) {
+                    ++j;
+                }
+                t.append((j - i) + "");
+                t.append(s.charAt(i));
+                i = j;
+            }
+            s = t.toString();
+        }
+        return s;
+    }
+}
