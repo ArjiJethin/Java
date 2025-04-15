e06041f45f51a1285c10d25ef95dd5d495fd4d33
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-15 18:51:37 +0530

    Add solution for finding the length of the longest valid parentheses substring

---
+++ Leet/Pager 1/Problem32.java
@@ -0,0 +1,30 @@
+
+public class Problem32 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "(()";
+        System.out.println(solution.longestValidParentheses(s));
+    }
+}
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        int[] f = new int[n + 1];
+        int ans = 0;
+        for (int i = 2; i <= n; ++i) {
+            if (s.charAt(i - 1) == ')') {
+                if (s.charAt(i - 2) == '(') {
+                    f[i] = f[i - 2] + 2;
+                } else {
+                    int j = i - f[i - 1] - 1;
+                    if (j > 0 && s.charAt(j - 1) == '(') {
+                        f[i] = f[i - 1] + 2 + f[j - 1];
+                    }
+                }
+                ans = Math.max(ans, f[i]);
+            }
+        }
+        return ans;
+    }
+}
