669e28059278f1ccc3b1da4978ff6a0e7d82fbab
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-15 18:51:59 +0530

    Add solution for validating a Sudoku board

---
+++ Leet/Pager 1/Problem36.java
@@ -0,0 +1,43 @@
+
+public class Problem36 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[][] board = {
+                { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                { '4', '.', '6', '8', '.', '3', '.', '.', '1' },
+                { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                { '.', '.', '2', '4', '1', '9', '.', '.', '.' },
+                { '3', '.', '.', '.', '8', '.', '.', '7', '.' }
+        };
+        System.out.println(solution.isValidSudoku(board)); // Output: true
+    }
+}
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] sub = new boolean[9][9];
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char c = board[i][j];
+                if (c == '.') {
+                    continue;
+                }
+                int num = c - '0' - 1;
+                int k = i / 3 * 3 + j / 3;
+                if (row[i][num] || col[j][num] || sub[k][num]) {
+                    return false;
+                }
+                row[i][num] = true;
+                col[j][num] = true;
+                sub[k][num] = true;
+            }
+        }
+        return true;
+    }
+}
