61f8c869282a763c8f73232c00f6b953bc76d538
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-15 18:53:03 +0530

    Add solution for multiplying two numbers as strings

---
+++ Leet/Pager 1/Problem43.java
@@ -0,0 +1,36 @@
+
+public class Problem43 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String num1 = "123";
+        String num2 = "456";
+        System.out.println(solution.multiply(num1, num2));
+    }
+}
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        if ("0".equals(num1) || "0".equals(num2)) {
+            return "0";
+        }
+        int m = num1.length(), n = num2.length();
+        int[] arr = new int[m + n];
+        for (int i = m - 1; i >= 0; --i) {
+            int a = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; --j) {
+                int b = num2.charAt(j) - '0';
+                arr[i + j + 1] += a * b;
+            }
+        }
+        for (int i = arr.length - 1; i > 0; --i) {
+            arr[i - 1] += arr[i] / 10;
+            arr[i] %= 10;
+        }
+        int i = arr[0] == 0 ? 1 : 0;
+        StringBuilder ans = new StringBuilder();
+        for (; i < arr.length; ++i) {
+            ans.append(arr[i]);
+        }
+        return ans.toString();
+    }
+}
