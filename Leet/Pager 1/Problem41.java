1fc526a05e23e2f5ad56c5ba90c4f8c477e24b79
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-15 18:52:54 +0530

    Add solution for finding the first missing positive number

---
+++ Leet/Pager 1/Problem41.java
@@ -0,0 +1,31 @@
+
+public class Problem41 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 3, 4, -1, 1 };
+        System.out.println(solution.firstMissingPositive(nums));
+    }
+}
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (i + 1 != nums[i]) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+}
