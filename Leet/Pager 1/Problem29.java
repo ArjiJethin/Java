d509a2e30a4b29ef672d45dbf6252061b202bd2e
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-15 18:51:24 +0530

    Add solution for dividing two integers

---
+++ Leet/Pager 1/Problem29.java
@@ -0,0 +1,44 @@
+
+public class Problem29 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int a = 10;
+        int b = 3;
+        System.out.println(solution.divide(a, b));
+        a = 7;
+        b = -3;
+        System.out.println(solution.divide(a, b));
+        a = -7;
+        b = 3;
+        System.out.println(solution.divide(a, b));
+        a = -7;
+        b = -3;
+        System.out.println(solution.divide(a, b));
+    }
+}
+
+class Solution {
+    public int divide(int a, int b) {
+        if (b == 1) {
+            return a;
+        }
+        if (a == Integer.MIN_VALUE && b == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean sign = (a > 0 && b > 0) || (a < 0 && b < 0);
+        a = a > 0 ? -a : a;
+        b = b > 0 ? -b : b;
+        int ans = 0;
+        while (a <= b) {
+            int x = b;
+            int cnt = 1;
+            while (x >= (Integer.MIN_VALUE >> 1) && a <= (x << 1)) {
+                x <<= 1;
+                cnt <<= 1;
+            }
+            ans += cnt;
+            a -= x;
+        }
+        return sign ? ans : -ans;
+    }
+}
