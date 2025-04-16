87e012c4604cd8268f29cd8135b56d174cb97c6d
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-16 12:24:09 +0530

    Add solution for generating a matrix in spiral order

---
+++ Leet/Pager 1/Problem59.java
@@ -0,0 +1,34 @@
+
+public class Problem59 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 3;
+        int[][] result = solution.generateMatrix(n);
+        for (int[] row : result) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] ans = new int[n][n];
+        int i = 0, j = 0, k = 0;
+        int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        for (int v = 1; v <= n * n; ++v) {
+            ans[i][j] = v;
+            int x = i + dirs[k][0], y = j + dirs[k][1];
+            if (x < 0 || y < 0 || x >= n || y >= n || ans[x][y] > 0) {
+                k = (k + 1) % 4;
+                x = i + dirs[k][0];
+                y = j + dirs[k][1];
+            }
+            i = x;
+            j = y;
+        }
+        return ans;
+    }
+}
