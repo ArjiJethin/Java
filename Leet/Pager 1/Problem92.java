4c9d1996ac75c44f5f7a65fed68ee60c69f59564
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-17 22:51:36 +0530

    Add solution for "Reverse Linked List II" problem

---
+++ Leet/Pager 1/Problem92.java
@@ -0,0 +1,38 @@
+public class Problem92 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        int left = 2;
+        int right = 4;
+        Solution solution = new Solution();
+        ListNode result = solution.reverseBetween(head, left, right);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head.next == null || left == right) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy;
+        for (int i = 0; i < left - 1; ++i) {
+            pre = pre.next;
+        }
+        ListNode p = pre;
+        ListNode q = pre.next;
+        ListNode cur = q;
+        for (int i = 0; i < right - left + 1; ++i) {
+            ListNode t = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = t;
+        }
+        p.next = pre;
+        q.next = cur;
+        return dummy.next;
+    }
+}
