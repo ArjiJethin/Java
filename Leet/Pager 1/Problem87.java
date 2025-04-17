dea99f724687ec51f3e849b8a9b51bc5e2c3e45a
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-17 22:51:11 +0530

    Refactor code to add a solution for the "Scramble String" problem

---
+++ Leet/Pager 1/Problem87.java
@@ -0,0 +1,41 @@
+public class Problem87 {
+    public static void main(String[] args) {
+        String s1 = "great";
+        String s2 = "rgeat";
+        Solution solution = new Solution();
+        boolean result = solution.isScramble(s1, s2);
+        System.out.println("Is Scramble: " + result);
+    }
+}
+
+class Solution {
+    private Boolean[][][] f;
+    private String s1;
+    private String s2;
+
+    public boolean isScramble(String s1, String s2) {
+        int n = s1.length();
+        this.s1 = s1;
+        this.s2 = s2;
+        f = new Boolean[n][n][n + 1];
+        return dfs(0, 0, n);
+    }
+
+    private boolean dfs(int i, int j, int k) {
+        if (f[i][j][k] != null) {
+            return f[i][j][k];
+        }
+        if (k == 1) {
+            return s1.charAt(i) == s2.charAt(j);
+        }
+        for (int h = 1; h < k; ++h) {
+            if (dfs(i, j, h) && dfs(i + h, j + h, k - h)) {
+                return f[i][j][k] = true;
+            }
+            if (dfs(i + h, j, k - h) && dfs(i, j + k - h, h)) {
+                return f[i][j][k] = true;
+            }
+        }
+        return f[i][j][k] = false;
+    }
+}
