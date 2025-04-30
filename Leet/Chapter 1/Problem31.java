b1a94ba856107dd8d9109bddfcf5f89b251bdf47
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-30 11:25:54 +0530

    Add solution for finding the next permutation of an array

---
+++ Leet/Chapter 1/Problem31.java
@@ -0,0 +1,41 @@
+
+public class Problem31 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 1, 2, 3 };
+        solution.nextPermutation(nums);
+        for (int x : nums) {
+            System.out.print(x + " ");
+        }
+    }
+}
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+        for (; i >= 0; --i) {
+            if (nums[i] < nums[i + 1]) {
+                break;
+            }
+        }
+        if (i >= 0) {
+            for (int j = n - 1; j > i; --j) {
+                if (nums[j] > nums[i]) {
+                    swap(nums, i, j);
+                    break;
+                }
+            }
+        }
+
+        for (int j = i + 1, k = n - 1; j < k; ++j, --k) {
+            swap(nums, j, k);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[j];
+        nums[j] = nums[i];
+        nums[i] = t;
+    }
+}
