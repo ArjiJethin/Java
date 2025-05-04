3e43433a01ec4914cba31132914863faa1e6b78b
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-05-04 13:19:22 +0530

    Add solution for spiral order traversal of a matrix

---
+++ Leet/Chapter 1/Problem54.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem54 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        List<Integer> result = solution.spiralOrder(matrix);
+        System.out.println(result); // Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+    }
+}
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[] dirs = { 0, 1, 0, -1, 0 };
+        int i = 0, j = 0, k = 0;
+        List<Integer> ans = new ArrayList<>();
+        boolean[][] vis = new boolean[m][n];
+        for (int h = m * n; h > 0; --h) {
+            ans.add(matrix[i][j]);
+            vis[i][j] = true;
+            int x = i + dirs[k], y = j + dirs[k + 1];
+            if (x < 0 || x >= m || y < 0 || y >= n || vis[x][y]) {
+                k = (k + 1) % 4;
+            }
+            i += dirs[k];
+            j += dirs[k + 1];
+        }
+        return ans;
+    }
+}
