5b2929aa8fc23c01226c2ed3c9fa189e41c37e1f
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-05-04 13:21:18 +0530

    Add solution for finding the minimum window substring

---
+++ Leet/Chapter 1/Problem76.java
@@ -0,0 +1,39 @@
+
+public class Problem76 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        String result = solution.minWindow(s, t);
+        System.out.println(result); // Output: "BANC"
+    }
+}
+
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] need = new int[128];
+        int[] window = new int[128];
+        int m = s.length(), n = t.length();
+        for (int i = 0; i < n; ++i) {
+            ++need[t.charAt(i)];
+        }
+        int cnt = 0, j = 0, k = -1, mi = 1 << 30;
+        for (int i = 0; i < m; ++i) {
+            ++window[s.charAt(i)];
+            if (need[s.charAt(i)] >= window[s.charAt(i)]) {
+                ++cnt;
+            }
+            while (cnt == n) {
+                if (i - j + 1 < mi) {
+                    mi = i - j + 1;
+                    k = j;
+                }
+                if (need[s.charAt(j)] >= window[s.charAt(j)]) {
+                    --cnt;
+                }
+                --window[s.charAt(j++)];
+            }
+        }
+        return k < 0 ? "" : s.substring(k, k + mi);
+    }
+}
